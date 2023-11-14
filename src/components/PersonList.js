cc5750be6cdccd79cb3ae9487c503207d010719d
EC2 Default User <ec2-user@ip-172-31-17-248.us-west-2.compute.internal>
2023-11-14 23:06:24 +0000

    Change API endpoint

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -7,7 +7,7 @@ export default class PersonList extends React.Component {
   }
 
   componentDidMount() {
-    axios.get(`https://q76xnqzf38.execute-api.us-west-2.amazonaws.com/prod/get-person`)
+    axios.get(`https://xajuaksi8l.execute-api.us-west-2.amazonaws.com/prod/get-todos`)
       .then(res => {
         const persons = res.data.body;
         this.setState({ persons });
